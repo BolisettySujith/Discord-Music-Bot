32eae12a0d7e68f92b35b8f4751bd305c1af9ec9
BolisettySujith <bsujith116@gmail.com>
2021-09-21 22:56:46 +0530

    add some interaction commands

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #Sujith
 import os
-
+import pyjokes
 import discord
+import datetime
+from pytube import YouTube
 
 TOKEN = "<TOKEN>"
 
@@ -15,7 +17,6 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
     if message.content.startswith('#CPL'):
         await message.channel.send("""Semester 3 Course Page
         https://amritauniv.sharepoint.com/sites/AIE2020/SitePages/Semester-3.aspx
@@ -31,7 +32,52 @@ async def on_message(message):
         https://amritauniv.sharepoint.com/sites/MIS32020batch
         Python for Machine Learning
         https://amritauniv.sharepoint.com/sites/19AIE205PythonforMachineLearningS3-2020Admissions/SitePages/19AIE205-Python-for-Machine-Learning.aspx?originalPath=aHR0cHM6Ly9hbXJpdGF1bml2LnNoYXJlcG9pbnQuY29tLzp1Oi9zLzE5QUlFMjA1UHl0aG9uZm9yTWFjaGluZUxlYXJuaW5nUzMtMjAyMEFkbWlzc2lvbnMvRVdOZmpaVWRJWHBOaDhDc2FnZFRzOUlCSlU4LVoxZC1jUmo2Q1pXTGxrTXVwZz9ydGltZT1aWE4zRGhGdDJVZw""")
-    elif message.content.startswith('#hello'):
+    elif message.content.startswith('#help'):
+        await message.channel.send("""
+        Share point course Page links
+        #CPL - For all cource pages links
+        #sp-os - Operating Systems
+        #sp-dsa - Data Structures and Algorithms
+        #sp-robotics - Introduction to Robotics
+        #sp-bio - Intelligence of Biological Systems
+        #sp-mat - Mathematics for intelligent Systems
+        #sp-ml - Python for Machine Learning
+
+        Interation commands:
+        hello, i am fine, what about you, what is your name, you are lucky
+        """)
+    elif message.content.startswith('hello'):
         await message.channel.send("Hai, how are u")
+    elif 'joke' in message.content:
+        await message.channel.send(pyjokes.get_joke())
+    elif 'current time' in message.content:
+        await message.channel.send(datetime.datetime.now().strftime('%I:%M %p'))
+    elif message.content.startswith('i am fine'):
+        await message.channel.send("ohh...")
+    elif message.content.startswith('download an youtube video'):
+        await message.channel.send("Sorry the feature is not implemented")
+    # elif message.content.startswith('!yt'):
+    #     link= message.content.replace("!yt", "")
+    #     yt=YouTube(link)
+    #     yt.streams.get_highest_resolution().download()
+    #     await message.channel.send("Downloaded the video")
+    elif message.content.startswith('what about you'):
+        await message.channel.send("i am superb, having no assignments and tests")
+    elif message.content.startswith('you are lucky'):
+        await message.channel.send("yeah")
+    elif message.content.startswith('what is your name'):
+        await message.channel.send("i am a AI BOT")
+    elif message.content.startswith('#sp-mat'):
+        await message.channel.send("https://amritauniv.sharepoint.com/sites/MIS32020batch")
+    elif message.content.startswith('#sp-ml'):
+        await message.channel.send("https://amritauniv.sharepoint.com/sites/19AIE205PythonforMachineLearningS3-2020Admissions/SitePages/19AIE205-Python-for-Machine-Learning.aspx?originalPath=aHR0cHM6Ly9hbXJpdGF1bml2LnNoYXJlcG9pbnQuY29tLzp1Oi9zLzE5QUlFMjA1UHl0aG9uZm9yTWFjaGluZUxlYXJuaW5nUzMtMjAyMEFkbWlzc2lvbnMvRVdOZmpaVWRJWHBOaDhDc2FnZFRzOUlCSlU4LVoxZC1jUmo2Q1pXTGxrTXVwZz9ydGltZT1aWE4zRGhGdDJVZw")
+    elif message.content.startswith('#sp-robotics'):
+        await message.channel.send("https://amritauniv.sharepoint.com/sites/19AIE201_Intro_to_Robotics/SitePages/ProjectHome.aspx")
+    elif message.content.startswith('#sp-bio'):
+        await message.channel.send("https://amritauniv.sharepoint.com/sites/IntelligenceofBiologicalSystems3?OR=Teams-HL&CT=1630639882730")
+    elif message.content.startswith('#sp-dsa'):
+        await message.channel.send("https://amritauniv.sharepoint.com/sites/19AIE203DataStructuresAlgorithms-2?OR=Teams-HL&CT=1630650595179")
+    elif message.content.startswith('#sp-os'):
+        await message.channel.send("https://amritauniv.sharepoint.com/sites/19AIE202OperatingSystems40/SitePages/19AIE202-Operating-Systems.aspx?web=1&OR=Teams-HL&CT=1630477344936")
 
 client.run(TOKEN)
